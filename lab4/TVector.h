2070cca2f5aa3764efbbb313058e60a584660726
Yanniku <borisov-yan@list.ru>
2021-11-28 19:57:22 +0300

    loaded lab4

--- lab4/TVector.h
+++ lab4/TVector.h
@@ -1,41 +1,135 @@
-#ifndef LAB1_TVECTOR_H
-#define LAB1_TVECTOR_H
+#pragma once
 
-#include <iostream>
-#include "TVectorItem.h"
-#include "square.h"
+#include <ostream>
 #include <memory>
 
-template <class T> class TVector {
+template <typename T>
+class TVector {
 public:
-
     TVector();
+    TVector(const TVector &);
 
-    TVector(const std::shared_ptr<TVector> p);
+    virtual ~TVector();
 
-    ~TVector();
+    inline size_t Length() const
+    {
+        return length_;
+    }
 
-    void InsertLast(const std::shared_ptr<T>& square);
+    inline bool Empty() const
+    {
+        return !length_;
+    }
 
-    void RemoveLast();
+    inline const std::shared_ptr<T> &operator[](const size_t index) const
+    {
+        return data_[index];
+    }
 
-    std::shared_ptr<T>& Last();
+    inline std::shared_ptr<T> Last() const
+    {
+        return data_[length_ - 1];
+    }
 
-    std::shared_ptr<T>& operator[] (const size_t idx);
+    void InsertLast(const std::shared_ptr<T> &);
+    void EmplaceLast(const T &&);
 
-    bool Empty();
+    void Remove(const size_t index);
 
-    size_t Length();
-
-    template<class A> friend std::ostream& operator<<(std::ostream& os, const TVector<A>& arr);
+    inline T RemoveLast()
+    {
+        return *data_[--length_];
+    }
 
     void Clear();
 
+    template <typename TF> friend std::ostream &operator<<(
+            std::ostream &, const TVector<TF> &);
 
 private:
-    size_t capacity;
-    std::shared_ptr<TVectorItem<T>[]> data;
-    size_t size;
+    void _Resize(const size_t new_capacity);
+
+    std::shared_ptr<T> *data_;
+    size_t length_, capacity_;
+
+    enum { INITIAL_CAPACITY = 32 };
 };
 
-#endif //LAB1_TVECTOR_H
+#include <cstdlib>
+
+template <typename T>
+TVector<T>::TVector()
+        : data_(new std::shared_ptr<T>[INITIAL_CAPACITY]),
+          length_(0), capacity_(INITIAL_CAPACITY) {}
+
+template <typename T>
+TVector<T>::TVector(const TVector &vector)
+        : data_(new std::shared_ptr<T>[vector.capacity_]),
+          length_(vector.length_), capacity_(vector.capacity_)
+{
+    std::copy(vector.data_, vector.data_ + vector.length_, data_);
+}
+
+template <typename T>
+TVector<T>::~TVector()
+{
+    delete[] data_;
+}
+
+// NOTE: C++ has no `realloc`, so this is a workaround:
+template <typename T>
+void TVector<T>::_Resize(const size_t new_capacity)
+{
+    std::shared_ptr<T> *newdata = new std::shared_ptr<T>[new_capacity];
+    std::copy(data_, data_ + capacity_, newdata);
+    delete[] data_;
+    data_ = newdata;
+    capacity_ = new_capacity;
+}
+
+#define _EXTEND_VECTOR_IF_NEEDED \
+   if (length_ >= capacity_) \
+      _Resize(capacity_ << 1);
+
+template <typename T>
+void TVector<T>::InsertLast(const std::shared_ptr<T> &item)
+{
+    _EXTEND_VECTOR_IF_NEEDED
+    data_[length_++] = item;
+}
+
+template <typename T>
+void TVector<T>::EmplaceLast(const T &&item)
+{
+    _EXTEND_VECTOR_IF_NEEDED
+    data_[length_++] = std::make_shared<T>(item);
+}
+
+#undef _EXTEND_VECTOR_IF_NEEDED
+
+template <typename T>
+void TVector<T>::Remove(const size_t index)
+{
+    std::copy(data_ + index + 1, data_ + length_, data_ + index);
+    --length_;
+}
+
+template <typename T>
+void TVector<T>::Clear()
+{
+    delete[] data_;
+    data_ = new std::shared_ptr<T>[INITIAL_CAPACITY];
+    length_ = 0;
+    capacity_ = INITIAL_CAPACITY;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const TVector<T> &vector)
+{
+    const size_t last = vector.length_ - 1;
+
+    for (size_t i = 0; i < vector.length_; ++i)
+        os << *vector.data_[i] << ((i != last) ? '\n' : '\0');
+
+    return os;
+}
