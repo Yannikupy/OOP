7918ac624a001a9b99a87ea0f3cee6162a162ba0
Yan <72080139+Yannikupy@users.noreply.github.com>
2022-01-21 15:53:36 +0300

    Add files via upload

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -10,10 +10,10 @@ int main () {
 
     Square c (std::cin);
     std:: cout << "The area of your figure is : " << c.Area() << std:: endl;
-    
+
     TVector<Square> vector;
-    std:: cout << "Is tree empty? " << vector.Empty() << std:: endl;
-    std:: cout << "And now, is tree empty? " << vector.Empty() << std:: endl;
+    std:: cout << "Is vector empty? " << vector.Empty() << std:: endl;
+    std:: cout << "And now, is vector empty? " << vector.Empty() << std:: endl;
     vector.InsertLast(std::make_shared<Square>(a));
     vector.InsertLast(std::make_shared<Square>(b));
     vector.InsertLast(std::make_shared<Square>(c));
